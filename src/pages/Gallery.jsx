ad98acdbbebb8db3e19cbe80f89b8241aa51c3e2
sakshikhargone <sakshi.khargone@gmail.com>
2025-03-02 23:52:25 +0530

    Add content to gallery page

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,7 +1,9 @@
 const Gallery = () => {
   return (
     <div className="row">
-      <div className="col-8"></div>
+      <div className="col-8">
+        <h4>Gallery page</h4>
+      </div>
     </div>
   );
 };
