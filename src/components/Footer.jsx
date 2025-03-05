69b0b9d3b6b2fb383d05f16103315aafa30edfaa
sakshikhargone <sakshi.khargone@gmail.com>
2025-03-05 23:51:21 +0530

    Add footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,7 @@
 const Footer = () => {
   return (
-    <div className="row">
-      <div className="colsm-10">
-        <p className="text-center">&#169; Coolspring</p>
-      </div>
+    <div>
+      <p className="text-center">&#169; Coolspring</p>
     </div>
   );
 };
