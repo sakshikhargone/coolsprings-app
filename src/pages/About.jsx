2edea6dc79f0377d76f701a7ef967578f6976fb7
sakshikhargone <sakshi.khargone@gmail.com>
2025-03-20 21:12:38 +0530

    Refresh about page with new details

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,24 +1,56 @@
-import { Link } from "react-router-dom";
-import GalleryCatlog from "../components/GalleryCatlog";
-
+import Waterpark from "../assets/waterpark.png";
+import styles from "../css/About.module.css";
 const About = () => {
   return (
-    <div className="row mt-5 ">
-      <div className="col-sm-10">
-        <h1 className="font-chewy-regular text-center">About Coolspring</h1>
-        <p className="mt-5">
-          Welcome to Coolspring, your ultimate online ticket booking system
-          designed exclusively for waterparks. Our platform ensures a fast,
-          secure, and hassle-free ticketing experience, allowing visitors to
-          book tickets, access detailed booking information, and track their
-          booking history with ease. At Coolspring, we prioritize user
-          convenience and security by integrating QR code-based entry, ensuring
-          a contactless and seamless check-in at the waterpark. Whether you're
-          planning a solo trip, a family outing, or a group visit, our system
-          provides a smooth and efficient booking process.
-        </p>
+    <div
+      className="row mt-5
+">
+      <div className="col-sm-12">
+        <div className="row">
+          <div className="col-10 col-sm-5">
+            <img
+              src={Waterpark}
+              alt="WaterPark"
+              className={`${styles.waterpark}`}
+            />
+          </div>
+          <div className="col-12 col-sm-6 ml-2 mt-4 ">
+            <h2 className="font-chewy-regular "> About Coolspring!</h2>
+            <p>
+              Welcome to <b>Coolspring Waterpark</b>, your ultimate destination
+              for fun, adventure, and relaxation! Nestled in a picturesque
+              location, Coolspring Waterpark offers a thrilling escape for
+              families, friends, and water enthusiasts of all ages.
+              <br /> From heart-pounding water slides and a massive wave pool to
+              a lazy river and dedicated kids’ play areas, we have attractions
+              for everyone.
+            </p>
+          </div>
+        </div>
+        <div className="row mt-5">
+          <div className="col">
+            <h4 className="font-chewy-regular ">Thrilling Water Attractions</h4>
+            <p>
+              Coolspring Waterpark, spanning over 30,000 sq. ft., offers
+              exhilarating water slides, a massive wave pool, and a relaxing
+              lazy river. From high-speed slides to interactive water play
+              zones, there's excitement for every age.
+              <br />
+              Our shaded cabanas and poolside cafés provide the perfect retreat.
+              Sip a chilled drink while enjoying the vibrant atmosphere or let
+              the kids explore the dedicated play area.
+            </p>
+          </div>
+        </div>
         <hr />
-        <Link to="about/gallery">Gallery</Link>
+        <section>
+          <h4 className="font-chewy-regular ">Stay at Coolspring Resort</h4>
+          <p>
+            Adjacent to the waterpark, Coolspring Resort offers modern
+            accommodations with luxury amenities, including a
+            temperature-controlled pool, gym, banquet halls, and more.
+          </p>
+        </section>
       </div>
     </div>
   );
