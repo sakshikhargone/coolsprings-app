262dc911eac336beddfb4e6b5c1c96d892c23c8c
sakshikhargone <sakshi.khargone@gmail.com>
2025-03-31 23:57:10 +0530

    Update gallery page by adding gallery carousel

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,10 +1,18 @@
+import GalleryCarousel from "../components/GalleryCarousel";
+import Heading from "../components/Heading";
+
 const Gallery = () => {
   return (
-    <div className="row">
-      <div className="col-8">
-        <h4>Gallery page</h4>
+    <div className="row ">
+      <div className="col-sm-11 offset-sm-2 ">
+        <Heading
+          content={"Explore the Coolspring Gallery"}
+          className="text-center"
+        />
+        <GalleryCarousel />
       </div>
     </div>
   );
 };
+
 export default Gallery;
