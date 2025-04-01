a16074e91bb55b2d8ae6046ec3953c53aac77f06
sakshikhargone <sakshi.khargone@gmail.com>
2025-04-01 23:43:17 +0530

    Add social media icons with links to Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,26 @@
+import { socialIcon } from "../util/socialIcon";
+
+import { Link } from "react-router-dom";
+import IconComponent from "./IconComponent";
+
 const Footer = () => {
   return (
-    <div>
-      <p className="text-center">&#169; Coolspring</p>
-      <p>Instagram</p>
+    <div className="row footer ">
+      <div className="col-sm-10 col-12 offset-sm-5 offset-3 ">
+        {socialIcon.map((icon, index) => {
+          return (
+            <Link
+              to={icon.url}
+              key={index}
+              className="px-1 icon">
+              <IconComponent
+                iconName={icon.icon}
+                size={1}
+              />
+            </Link>
+          );
+        })}
+      </div>
     </div>
   );
 };
