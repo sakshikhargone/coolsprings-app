7e56693ee1320cf41d363a3c9baab1ff4e2dd8b3
sakshikhargone <sakshi.khargone@gmail.com>
2025-04-02 17:44:42 +0530

    Create reusable heading component for page titles.

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,4 +1,15 @@
-const Heading = () => {
-  return <></>;
+import styles from "../css/Heading.module.css";
+const Heading = ({ content }) => {
+  return (
+    <div className="row">
+      <div className="col-sm-10 ">
+        <h1
+          className={`font-chewy-regular mb-sm-5 
+        mt-sm-5 mt-3 text-center heading ${styles.title}`}>
+          {content}
+        </h1>
+      </div>
+    </div>
+  );
 };
 export default Heading;
