f26fd7d95420496848851c17dbb6878edf77837f
sakshikhargone <sakshi.khargone@gmail.com>
2025-04-18 23:42:21 +0530

    Create an array for social media icons

---
+++ src/util/socialIcon.js
@@ -0,0 +1,23 @@
+import { mdiInstagram } from "@mdi/js";
+import { mdiTwitter } from "@mdi/js";
+import { mdiFacebook } from "@mdi/js";
+import { mdiWhatsapp } from "@mdi/js";
+
+export const socialIcon = [
+  {
+    url: "https://www.instagram.com/accounts/login/?hl=en",
+    icon: mdiInstagram,
+  },
+  {
+    url: "https://x.com/i/flow/login?redirect_after_login=%2Fexplore",
+    icon: mdiTwitter,
+  },
+  {
+    url: "https://www.facebook.com/",
+    icon: mdiFacebook,
+  },
+  {
+    url: "https://web.whatsapp.com/",
+    icon: mdiWhatsapp,
+  },
+];
