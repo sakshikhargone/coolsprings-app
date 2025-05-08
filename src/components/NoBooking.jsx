833539e4a3824693337a6358ebf51aecb42dc559
sakshikhargone <sakshi.khargone@gmail.com>
2025-05-08 23:12:26 +0530

    Render NoBooking UI for no active bookings

---
+++ src/components/NoBooking.jsx
@@ -0,0 +1,10 @@
+const NoBooking = () => {
+  return (
+    <div className="row mt-sm-5 mt-5">
+      <div className="col col-sm-8 offset-3 offset-sm-4 mt-5 mt-sm-5">
+        <h1 className="font-chewy-regular ">No Booking...</h1>
+      </div>
+    </div>
+  );
+};
+export default NoBooking;
