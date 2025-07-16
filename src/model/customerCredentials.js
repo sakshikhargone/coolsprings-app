afa73c4b74d2bcee93072fb12f94551a030efdc0
sakshikhargone <sakshi.khargone@gmail.com>
2025-07-16 14:25:22 +0530

    Add CustomerCredentials model to define customer credentials

---
+++ src/model/customerCredentials.js
@@ -0,0 +1,8 @@
+export class CustomerCredentials {
+  constructor(customerPhone, customerEmail, customerPassword) {
+    this.customerPhone = customerPhone;
+    this.customerEmail = customerEmail;
+
+    this.customerPassword = customerPassword;
+  }
+}
---
+++ src/util/galleryItems.js
@@ -0,0 +1,28 @@
+import Image1 from "../assets/img1.jpg";
+import Image2 from "../assets/img2.jpg";
+import Image3 from "../assets/img3.jpg";
+import Image4 from "../assets/img4.jpg";
+import Image5 from "../assets/img5.jpg";
+
+export const galleryItems = [
+  {
+    url: Image1,
+    text: "Image1",
+  },
+  {
+    url: Image2,
+    text: "Image2",
+  },
+  {
+    url: Image3,
+    text: "Image3",
+  },
+  {
+    url: Image4,
+    text: "Image4",
+  },
+  {
+    url: Image5,
+    text: "Image5",
+  },
+];
