f5e9f57d16c751938878553ac5435c8570442bc1
Sakshi Gupta <141125644+sakshikhargone@users.noreply.github.com>
2025-07-16 14:37:38 +0530

    Update customerCredentials.js

--- src/model/customerCredentials.js
+++ src/model/customerCredentials.js
@@ -2,7 +2,6 @@ export class CustomerCredentials {
   constructor(customerPhone, customerEmail, customerPassword) {
     this.customerPhone = customerPhone;
     this.customerEmail = customerEmail;
-
     this.customerPassword = customerPassword;
   }
 }
